aa50cabb0629a32e9d61230a02635c6b6e1bf1fa
Diego Dario <diegodario88@gmail.com>
2019-12-28 18:10:12 -0300

    ajustees no setInterval

--- src/app/services/dataService.js
+++ src/app/services/dataService.js
@@ -24,30 +24,30 @@ const verificaData = async (diaAtual) => {
         plantaoAtual = await Repository.getByStatusSemanal();
         diaPlantao = plantaoAtual.escalaSemanal.getDate();
         if (diaAtual > diaPlantao) {
-            console.info(`O dia mudou para ${diaAtual}. Alteração do plantão em andamento...`);
-            console.info(`Plantão anterior: ${plantaoAtual.name} alterando para o próximo.`);
+            console.debug(`O dia mudou para ${diaAtual}. Alteração do plantão em andamento...`);
+            console.debug(`Plantão anterior: ${plantaoAtual.name} alterando para o próximo.`);
             plantaoService.proximoPlantao(EscalaEnum.semanal, plantaoAtual);
         }
-        return console.warn(`Dia do plantão: ${diaPlantao} *** Dia atual: ${diaAtual}`);
+        return console.info(`Dia do plantão: ${diaPlantao} *** Dia atual: ${diaAtual}`);
 
-    } //SÁBADAL 
+    } //SABADAL 
     else if (dia === sabado) {
         plantaoAtual = await Repository.getByStatusSabadal();
         diaPlantao = plantaoAtual.escalaSabado.getDate();
         if (diaAtual > diaPlantao) {
-            console.info(`O dia mudou para ${diaAtual}. Alteração do plantão em andamento...`);
-            console.info(`Plantão anterior: ${plantaoAtual.name} alterando para o próximo.`);
+            console.debug(`O dia mudou para ${diaAtual}. Alteração do plantão em andamento...`);
+            console.debug(`Plantão anterior: ${plantaoAtual.name} alterando para o próximo.`);
             plantaoService.proximoPlantao(EscalaEnum.sabadal, plantaoAtual);
         }
-        return console.warn(`Dia do plantão: ${diaPlantao} *** Dia atual: ${diaAtual}`);
+        return console.info(`Dia do plantão: ${diaPlantao} *** Dia atual: ${diaAtual}`);
 
     } //DOMINGAL
     else {
         plantaoAtual = await Repository.getByStatusDomingal();
         diaPlantao = plantaoAtual.escalaDomingo.getDate();
         if (diaAtual > diaPlantao) {
-            console.info(`O dia mudou para ${diaAtual}. Alteração do plantão em andamento...`);
-            console.info(`Plantão anterior: ${plantaoAtual.name} alterando para o próximo.`);
+            console.debug(`O dia mudou para ${diaAtual}. Alteração do plantão em andamento...`);
+            console.debug(`Plantão anterior: ${plantaoAtual.name} alterando para o próximo.`);
             plantaoService.proximoPlantao(EscalaEnum.domingal, plantaoAtual);
         }
         return console.info('dia continua o mesmo do plantão ' + diaPlantao);
@@ -58,8 +58,8 @@ const verificaData = async (diaAtual) => {
 const minutos = 1;
 const intervalo = minutos * 60 * 2000;
 
-(setInterval(function () {
+setInterval(() => {
     console.warn('Monitorando Data -->');
     verificaData(new Date().getDate()).catch(console.warn);
-}, intervalo));
+}, intervalo);
 
