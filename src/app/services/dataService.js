3e9a002e3028a242a8ad81c4442fbacc79a3d5f4
Diego Dario <diegodario88@gmail.com>
2020-01-04 03:24:27 -0300

    refactor monitoraData

--- src/app/services/dataService.js
+++ src/app/services/dataService.js
@@ -8,58 +8,57 @@ const minutos = 120;
 const intervalo = minutos * 60 * 1000;
 
 setInterval(() => {
-    console.warn(`Monitorando Data --> 
+    console.warn(`Monitorando Data atual --> 
     ${moment().utcOffset('-03:00').format('DD/MM/YYYY - H:mm:ss A')}`);
-    const diaAtual = moment().utcOffset('-03:00').format('DD/MM/YYYY');
-    monitoraData(diaAtual).catch(console.warn);
+    const diaAtual = moment().utcOffset('-03:00');
+    monitoraEscala(diaAtual).catch(console.warn);
 }, intervalo);
 
-const monitoraData = async (diaAtual) => {
+const monitoraEscala = async (diaAtual) => {
 
+    const EscalaEnum = Object.freeze({ "semanal": 1, "sabadal": 2, "domingal": 3, })
     let plantaoAtual = null;
-    let diaPlantao = null;
     const dia = moment().utcOffset('-03:00').day();
     const sabado = 6;
     const domingo = 0;
-    const EscalaEnum = Object.freeze({ "semanal": 1, "sabadal": 2, "domingal": 3, })
 
     //SEMANAL
     if (dia > domingo && dia < sabado) {
         plantaoAtual = await Repository.getByStatusSemanal();
-        diaPlantao = moment(plantaoAtual.escalaSemanal).utcOffset('-03:00').format('DD/MM/YYYY');
-        if (diaAtual > diaPlantao) {
-            console.info(`O dia mudou para ${diaAtual}. Alteração do plantão em andamento...`);
-            console.info(`Plantão anterior: ${plantaoAtual.name} alterando para o próximo.`);
-            plantaoService.proximoPlantao(EscalaEnum.semanal, plantaoAtual);
-            return;
-        }
-        console.info(`Dia do plantão: ${diaPlantao} *** Dia atual: ${diaAtual}`);
+        const { escalaSemanal } = plantaoAtual;
+        verificaDataPlantao(plantaoAtual, diaAtual, EscalaEnum.semanal, escalaSemanal);
 
     } //SABADAL 
     else if (dia === sabado) {
         plantaoAtual = await Repository.getByStatusSabadal();
-        diaPlantao = plantaoAtual.escalaSabado.getDate();
-        if (diaAtual > diaPlantao) {
-            console.debug(`O dia mudou para ${diaAtual}. Alteração do plantão em andamento...`);
-            console.debug(`Plantão anterior: ${plantaoAtual.name} alterando para o próximo.`);
-            plantaoService.proximoPlantao(EscalaEnum.sabadal, plantaoAtual);
-            return;
-        }
-        console.info(`Dia do plantão: ${diaPlantao} *** Dia atual: ${diaAtual}`);
+        const { escalaSabado } = plantaoAtual;
+        verificaDataPlantao(plantaoAtual, diaAtual, EscalaEnum.sabadal, escalaSabado);
 
     } //DOMINGAL
     else {
         plantaoAtual = await Repository.getByStatusDomingal();
-        diaPlantao = plantaoAtual.escalaDomingo.getDate();
-        if (diaAtual > diaPlantao) {
-            console.debug(`O dia mudou para ${diaAtual}. Alteração do plantão em andamento...`);
-            console.debug(`Plantão anterior: ${plantaoAtual.name} alterando para o próximo.`);
-            plantaoService.proximoPlantao(EscalaEnum.domingal, plantaoAtual);
-            return;
-        }
-        console.info(`Dia do plantão: ${diaPlantao} *** Dia atual: ${diaAtual}`);
+        const { escalaDomingo } = plantaoAtual;
+        verificaDataPlantao(plantaoAtual, diaAtual, EscalaEnum.domingal, escalaDomingo);
     }
 }
 
-module.exports = { monitoraData };
+const verificaDataPlantao = (plantaoAtual, diaAtual, EscalaEnum, dataEscala) => {
+    const diaPlantao = moment(dataEscala)
+        .utcOffset('-03:00');
+
+    if (diaAtual.year() > diaPlantao.year() && diaAtual.dayOfYear() > diaPlantao.dayOfYear()) {
+        //Troca plantão
+        logInfo(plantaoAtual.name, diaAtual)
+        return plantaoService.proximoPlantao(EscalaEnum, plantaoAtual);
+    }
+    console.info(`Data do plantão: ${diaPlantao.format('DD/MM/YYYY - H:mm:ss A')}`);
+
+}
+
+const logInfo = (name, diaAtual) => {
+    console.info(`O dia mudou para ${diaAtual.format('DD/MM/YYYY')}. Alteração do plantão em andamento...`);
+    console.info(`Plantão anterior: ${name} alterando para o próximo.`);
+}
+
+module.exports = { monitoraEscala };
 
